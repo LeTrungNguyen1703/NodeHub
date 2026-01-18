e11622da2d0c8e441f5d5a6f56e18f0e12d08f85
Nguyen <nguyenc2300110@student.ctu.edu.vn>
2026-01-18 14:13:23 +0700

    feat: add Cloudinary configuration for testing and update application properties

--- src/test/java/com/modulith/auctionsystem/TestcontainersConfiguration.java
+++ src/test/java/com/modulith/auctionsystem/TestcontainersConfiguration.java
@@ -41,4 +41,9 @@ class TestcontainersConfiguration {
         return jwtDecoder;
     }
 
+    @Bean
+    public com.cloudinary.Cloudinary cloudinary() {
+        return mock(com.cloudinary.Cloudinary.class);
+    }
+
 }
